394d42115382a3fe2369997f52cee30e481d9c4e
Sprite137 <3304424497@qq.com>
2024-08-04 23:25:31 +0800

    20240804

--- fourth/src/P128.java
+++ fourth/src/P128.java
@@ -1,9 +1,32 @@
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * @author: xuzhi
  * @data: 2024/8/4
  */
 public class P128 {
-    public int longestConsecutive(int[] nums) {
+    public static int longestConsecutive(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for(int num :nums){
+            set.add(num);
+        }
 
+        int ans = 0;
+        for(int num :nums){
+            if(set.contains(num-1)){
+                continue;
+            }
+            int temp = num;
+            while(set.contains(temp+1)){
+                temp++;
+            }
+            ans = Math.max(temp-num+1,ans);
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.err.println(longestConsecutive(new int[]{0,3,7,2,5,8,4,6,0,1}));
     }
 }

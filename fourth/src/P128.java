19d6f86e11eec8eda5c5f66c49f4160763fadec0
Sprite137 <3304424497@qq.com>
2024-08-04 23:08:54 +0800

    20240804

---
+++ fourth/src/P128.java
@@ -0,0 +1,9 @@
+/**
+ * @author: xuzhi
+ * @data: 2024/8/4
+ */
+public class P128 {
+    public int longestConsecutive(int[] nums) {
+
+    }
+}
--- fourth/src/P49.java
+++ fourth/src/P49.java
@@ -1,9 +1,22 @@
-import java.util.List;
+import java.util.*;
 
 public class P49 {
     public List<List<String>> groupAnagrams(String[] strs) {
 
-//        for(int )
+
+        Map<String,List<String>> map = new HashMap<>();
+        for(String str :strs ){
+            int[] bits = new int[26];
+            for(char a: str.toCharArray()){
+                bits[a-'a']++;
+            }
+            List<String> temp = map.getOrDefault(Arrays.toString(bits),new ArrayList<>());
+            temp.add(str);
+            map.put(Arrays.toString(bits),temp);
+        }
+
+        return new ArrayList<>(map.values());
+
 
     }
 }

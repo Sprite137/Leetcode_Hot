fe702a6b53d92a8bf82ce92f6c09e012004650d8
xuzhi <zhizxu@tencent.com>
2024-08-07 19:59:55 +0800

    20240807

--- fourth/src/P438.java
+++ fourth/src/P438.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -7,7 +10,53 @@ import java.util.List;
  */
 public class P438 {
     // todo
-//    public List<Integer> findAnagrams(String s, String p) {
-//
-//    }
+    public List<Integer> findAnagrams(String s, String p) {
+        int[] bits = new int[26];
+
+        if(s.equals(p)){
+            return new ArrayList<>(Collections.singletonList(0));
+        }
+        if(s.length() <p.length()){
+            return null;
+        }
+        for(char a : p.toCharArray()){
+            bits[a-'a']--;
+        }
+
+        List<Integer> ans = new ArrayList<>();
+
+        int left = 0, right = 0;
+        while(left != s.length()){
+            // 如果无效，right++
+            while( right < s.length() && !isValid(bits)){
+                bits[s.charAt(right++)-'a']++;
+            }
+
+            // 如果有效 left++
+            while( left <= right && isValid(bits)){
+                bits[s.charAt(left)-'a']--;
+                ans.add(left);
+                left++;
+            }
+
+            if(right == s.length()){
+                return ans;
+            }
+        }
+        return ans;
+    }
+
+    public boolean isValid(int[] bits){
+        for(int i : bits){
+            if(i < 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        P438 x = new P438();
+        System.err.println(x.findAnagrams("cbaebabacd", "abc"));
+    }
 }
